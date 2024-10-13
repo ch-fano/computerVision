26896ef2bfccf955eb4d6c903663027a720b0c0d
christofer <christofer.fano8@gmail.com>
2024-10-13 16:48:19 +0200

    Created the recognition pipeline class

--- utilities/class_discovery.py
+++ utilities/class_discovery.py
@@ -1,163 +1,31 @@
 import os
-import numpy as np
-import torch
-import cv2
+from recognition_pipeline import RecognitionPipeline
 
-
-def get_best_class(probabilities):
+def test_best_class(rp, directory_path):
     """
-    This function extract the label with the highest probability.
-    
-    :param probabilities: List of [label, probability].
-    :return: The [label, probability] with the highest probability.
+    This function recognise the objects in the images of the directory, memorize and print only the results
+    of the class with the highest probability.
+
+    :param rp: The instance of the recognition pipeline to use.
+    :param directory_path: The path to the directory to test.
     """
-    best = []
-    for elem in probabilities:
-
-        if len(best) == 0 or elem[1] > best[1]:
-            best = elem
-
-    if len(best) != 0:
-        print(best[1])
-    # Return class name and with the best confidence
-    return best
-
-
-def yolo_detection(img_path):
-    """
-    This function process the image with the trained yolo model and returns the predicted class with
-    the highest confidence.
-    
-    :param img_path: The path to the image to recognize.
-    :return: The class with the highest probability.
-    """
-    
-    # Insert the path to the yolov5 directory
-    model_dir = '/home/christofer/PycharmProjects/computerVision/yolov5'
-
-    # Insert the path to the custom weights
-    custom_weights = '/home/christofer/PycharmProjects/computerVision/yolov5/runs/train/3C_80E_DA_3_2_MAX/weights/best.pt'
-
-    model = torch.hub.load(model_dir,'custom', source='local', path=custom_weights, force_reload=True)
-    model.conf = 0.25  # confidence threshold
-    model.iou = 0.45   # IoU threshold
-    model.classes = None  # filter by class
-    model.img_size = 640  # set image size to 640x640 pixels
-
-    # Perform inference
-    results = model(img_path)
-
-    # results.print()
-    # results.save()   # Save results to disk (runs in YOLOv5/runs/detect)
-    # results.show()     # Shows the image and the bbox
-
-    # Access detailed results if needed
-    df = results.pandas().xyxy[0]  # Get bounding boxes as pandas DataFrame
-    #print(df)
-
-    # Return class name and with the best confidence
-    return get_best_class(df[['name', 'confidence']].values.tolist())
-
-
-def get_max_value(dtype):
-    """
-    This function returns the maximum brightness value of a given image type.
-    
-    :param dtype: The image type
-    :return: The maximum brightness value the given image type.
-    """
-    
-    if np.issubdtype(dtype, np.integer):
-        return np.iinfo(dtype).max
-    elif np.issubdtype(dtype, np.floating):
-        return 1.0  # Typically for floating point images
-    else:
-        raise ValueError("Unsupported dtype")
-
-
-def get_brightness(img_path):
-    """
-    This function returns the brightness expressed form 0 to 1 of a given image.
-    
-    :param img_path: The path to the image to calculate the brightness.
-    :return: The brightness of the image.
-    """
-    image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-
-    if image is None:
-        print(f"Warning: Image not found or could not be loaded: {img_path}")
-        return None
-
-    # Determine the max possible value for the image's dtype
-    max_val = get_max_value(image.dtype)
-
-    # Convert the image to grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    return np.mean(gray_image) / max_val
-
-
-def is_illumination(img_path):
-    """
-    This function returns the probability of the given image to be an instance of the 'illumination' class.
-    
-    :param img_path: The image to compute the probability.
-    :return: The probability of the image to be an instance of the 'illumination' class.
-    """
-    
-    threshold = 0.30
-    brightness = get_brightness(img_path)
-
-    if brightness is None:
-        return 0  # Assuming no illumination class if the image can't be processed
-
-    if brightness <= threshold:
-        return 1 - (brightness/threshold)
-
-    return 0
-
-def recognition_pipeline(image_path):
-    """
-    This function implements the recognition pipeline of an image, it extracts the probabilities
-    of the image to be an instance of each class.
-    
-    :param image_path: The image to recognize.
-    :return: The class with the highest probability and the list of the classes of which the image could be an instance.
-    """
-    
-    classes = []
-
-    illumination_prob = is_illumination(image_path)
-
-    if illumination_prob:
-        classes.append(['illuminazione_brightness', illumination_prob])
-
-    labels = yolo_detection(image_path)
-    if len(labels) != 0:
-        classes.append(labels)
-
-    return get_best_class(classes), classes
-
-if __name__ == '__main__':
-
     valid_image_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.tiff', '.tif', '.gif']
-
-    test_dir = '/home/christofer/Desktop/second_images/159_rifiuti_abbandonati'
     dir_classes = {'undefined': 0}
     tot_imgs = 0
 
-    for image_path in os.listdir(test_dir):
+    for image_path in os.listdir(directory_path):
 
-        full_path = os.path.join(test_dir, image_path)
+        full_path = os.path.join(directory_path, image_path)
 
         if os.path.splitext(full_path)[1].lower() in valid_image_extensions:
             print(f"\nProcessing image: {full_path}")
             tot_imgs += 1
 
-            best, classes = recognition_pipeline(full_path)
+            classes = rp.recognize(full_path)
+            best = classes[0]
 
             if len(best) == 0:
-                dir_classes['undefined'] +=1
+                dir_classes['undefined'] += 1
             elif best[0] in dir_classes:
                 dir_classes[best[0]] += 1
             else:
@@ -165,12 +33,76 @@ if __name__ == '__main__':
         else:
             print(f"Skipping non-image file: {full_path}")
 
-
     print(f'Total images: {tot_imgs}')
     print('Predicted results: ')
     for key, value in dir_classes.items():
         print(f'- {key}: {value}')
 
-    #best, classes = test_img(image_path)
-    #print('Classes: ', classes)
-    #print('Best: ', best)
+
+def test_set_classes(rp, directory_path, classes_txt, verbose=False):
+    valid_image_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.tiff', '.tif', '.gif']
+
+    class_labels = list()
+    with open(classes_txt, 'r') as f:
+        for line in f:
+            class_labels.append(line.strip())
+
+    tot_imgs = 0
+    correct = 0
+    proposed=0
+    for image_path in os.listdir(directory_path):
+
+        full_path = os.path.join(directory_path, image_path)
+        name_ext = os.path.splitext(full_path)
+        true_classes = set()
+
+        if name_ext[1].lower() in valid_image_extensions:
+
+            print(f"\n\nExtracting annotation: {name_ext[0]}.txt")
+            with open(name_ext[0]+'.txt', 'r') as f:
+                for line in f:
+                    class_id = int(line.split()[0])
+                    true_classes.add(class_labels[class_id])
+
+            print(f"Processing image: {full_path}")
+            tot_imgs += 1
+
+            predicted_classes = rp.recognize(full_path)
+            predicted_classes_name =  set([elem[0] for elem in predicted_classes])
+            proposed += len(predicted_classes_name)
+
+            # Calculate if it is correct or not
+            recognized = True
+            for c in true_classes:
+                if c not in predicted_classes_name:
+                    recognized = False
+
+            if recognized:
+                correct += 1
+
+            if verbose:
+                print("- True classes: ", end=" ")
+                for c in true_classes:
+                    print(c, end=" ")
+
+                print("\n- Predicted classes: ", end=" ")
+                for c in predicted_classes_name:
+                    print(c, end=" ")
+        #else:
+        #    print(f"\nSkipping non-image file: {full_path}")
+
+    print(f'\n\nTotal images: {tot_imgs}')
+    print(f'Correct predictions: {correct}')
+    print(f'Total number of classes: {len(class_labels)}')
+    print(f'Average number of proposed predictions: {proposed/tot_imgs}')
+
+
+if __name__ == '__main__':
+
+    yolov5_dir = '/home/christofer/PycharmProjects/computerVision/yolov5'
+    weights_path = '/home/christofer/PycharmProjects/computerVision/yolov5/runs/train/10C_40E_DA_3_2_MAX/weights/best.pt'
+    rp = RecognitionPipeline(yolov5_dir=yolov5_dir, custom_weights=weights_path)
+
+    dir_path = '/home/christofer/Desktop/test'
+    class_file = '/home/christofer/PycharmProjects/computerVision/classes.txt'
+    test_set_classes(rp, dir_path, class_file, verbose=True)
---
+++ utilities/recognition_pipeline.py
@@ -0,0 +1,112 @@
+import numpy as np
+import torch
+import cv2
+
+class RecognitionPipeline:
+    def __init__(self, yolov5_dir, custom_weights):
+        print("Initializing the yolov5 model of the recognition pipeline")
+        self.model = torch.hub.load(yolov5_dir, 'custom', source='local', path=custom_weights, force_reload=True)
+        self.model.conf = 0.01  # confidence threshold (before was 0.25)
+        self.model.iou = 0.45  # IoU threshold
+        self.model.classes = None  # filter by class
+        self.model.img_size = 640  # set image size to 640x640 pixels
+
+    def yolo_detection(self, img_path):
+        """
+        This function process the image with the trained yolo model and returns the predicted class with
+        the highest confidence.
+
+        :param img_path: The path to the image to recognize.
+        :return: List of [class, probability] of which the image could be an instance.
+        """
+
+        # Perform inference
+        results = self.model(img_path)
+
+        # results.print()
+        # results.save()     # Save results to disk (runs in YOLOv5/runs/detect)
+        # results.show()     # Shows the image and the bbox
+
+        # Access detailed results if needed
+        df = results.pandas().xyxy[0]  # Get bounding boxes as pandas DataFrame
+        # print(df)
+
+        # Return class name and with the best confidence
+        return df[['name', 'confidence']].values.tolist()
+
+    def get_imgtype_max_value(self, dtype):
+        """
+        This function returns the maximum brightness value of a given image type.
+
+        :param dtype: The image type
+        :return: The maximum brightness value the given image type.
+        """
+
+        if np.issubdtype(dtype, np.integer):
+            return np.iinfo(dtype).max
+        elif np.issubdtype(dtype, np.floating):
+            return 1.0  # Typically for floating point images
+        else:
+            raise ValueError("Unsupported dtype")
+
+    def get_brightness(self, img_path):
+        """
+        This function returns the brightness expressed form 0 to 1 of a given image.
+
+        :param img_path: The path to the image to calculate the brightness.
+        :return: The brightness of the image.
+        """
+        image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+
+        if image is None:
+            print(f"Warning: Image not found or could not be loaded: {img_path}")
+            return None
+
+        # Determine the max possible value for the image's dtype
+        max_val = self.get_imgtype_max_value(image.dtype)
+
+        # Convert the image to grayscale
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        return np.mean(gray_image) / max_val
+
+    def is_illumination(self, img_path, threshold):
+        """
+        This function returns the probability of the given image to be an instance of the 'illumination' class.
+
+        :param img_path: The image to compute the probability.
+        :param threshold: The brightness threshold, if the brightness of the image is under the threshold the picture could
+                          be an instance of the 'illumination' class.
+        :return: The probability of the image to be an instance of the 'illumination' class.
+        """
+
+        brightness = self.get_brightness(img_path)
+
+        if brightness is None:
+            return 0  # Assuming no illumination class if the image can't be processed
+
+        if brightness <= threshold:
+            return 1 - (brightness / threshold)
+
+        return 0
+
+    def recognize(self, image_path, brightness_threshold=0.30):
+        """
+        This function implements the recognition pipeline of an image, it extracts the probabilities
+        of the image to be an instance of each class.
+
+        :param image_path: The image to recognize.
+        :param brightness_threshold: The brightness threshold, if the brightness of an image is under the threshold the picture could
+                          be an instance of the 'illumination' class.
+        :return: The class with the highest probability and the list of the classes of which the image could be an instance.
+        """
+
+        classes = self.yolo_detection(image_path)
+
+        illumination_prob = self.is_illumination(image_path, brightness_threshold)
+
+        if illumination_prob:
+            classes.append(['illuminazione', illumination_prob])
+
+        classes.sort(key=lambda x: x[1], reverse=True)
+        return classes
